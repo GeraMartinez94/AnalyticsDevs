864658221cc295da47c8256d6203814f30fe9cee
GeraMartinez94 <geramartinez450@gmail.com>
2025-07-03 16:58:27 -0300

    Update

--- src/app/contacto/contacto.ts
+++ src/app/contacto/contacto.ts
@@ -1,7 +1,10 @@
 
 import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Si usas ngIf, ngFor, etc.
-
+import { ActivatedRoute } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators'
 // Módulos de Angular Material para el formulario
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -21,8 +24,33 @@ import { MatButtonModule } from '@angular/material/button';
    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ContactoComponent  implements OnInit {
-  constructor() { }
+  private fragmentSubscription: Subscription | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private viewportScroller: ViewportScroller
+  ) {}
 
   ngOnInit(): void {
+    // Suscribirse a los cambios del fragmento en la URL
+    this.fragmentSubscription = this.route.fragment
+      .pipe(
+        // Filtramos para asegurarnos de que solo actuamos cuando hay un fragmento
+        filter(fragment => !!fragment)
+      )
+      .subscribe(fragment => {
+        // Aumentamos el tiempo de espera para dar más tiempo a la renderización
+        // Puedes ajustar este valor si es necesario (ej. 500ms, 700ms)
+        setTimeout(() => {
+          this.viewportScroller.scrollToAnchor(fragment as string);
+        }, 300); // Aumentado a 300ms
+      });
+  }
+
+  ngOnDestroy(): void {
+    // Es importante desuscribirse para evitar fugas de memoria
+    if (this.fragmentSubscription) {
+      this.fragmentSubscription.unsubscribe();
+    }
   }
 }
